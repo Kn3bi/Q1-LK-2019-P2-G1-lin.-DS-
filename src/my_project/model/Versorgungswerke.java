50219530db060c78309e2238f5cf4f275ec52160
Joshua Ressler <joshua.ressler0119@gmail.com>
2020-01-15 22:41:39 +0100

    22 Commit, Häuser und Hotels können gebaut und Miete gezahlt werden.

---
+++ src/my_project/model/Versorgungswerke.java
@@ -0,0 +1,7 @@
+package my_project.model;
+
+public class Versorgungswerke extends AllgemeinesFeld {
+    public Versorgungswerke(int breite, int hoehe, double x, double y, String name) {
+        super(breite, hoehe, x, y, name);
+    }
+}
